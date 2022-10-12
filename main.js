1ccb9ab91b11cfbf50d9648de302d63dd4aa7dbb
Oliver <oli2000@outlook.dk>
2022-10-12 10:51:42 +0200

    colors

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-const inl = document.querySelector('#invoicelines').getContext('2d');
+const inl = document.querySelector('#invoice').getContext('2d');
 const chart = new Chart(inl, {
     type: 'bar',
     data: {
         labels: ['USA🇺🇸', 'Canada🇨🇦', 'France🇫🇷', 'Brazil🇧🇷', 'Germany🇩🇪', 'Denmark🇩🇰'],
         datasets: [{
+            label: 'Invoice Total',
+            data: [4667, 2689, 1722, 1677, 1392, 334],
+            backgroundColor: 'rgba(0, 0, 0, 0.8)'
+        }, {
             label: 'Invoice Lines',
             data: [494, 304, 190, 190, 152, 38],
-            backgroundColor: ['red', 'lightgreen', 'lightblue','yellow','lime','pink',],
+            backgroundColor: 'rgba(50, 150, 150, 0.8)'
         }]
-    }
-});
-
-const int = document.querySelector('#invoicetotal').getContext('2d');
-const chart2 = new Chart(int, {
-    type: 'bar',
-    data: {
-        labels: ['USA🇺🇸', 'Canada🇨🇦', 'France🇫🇷', 'Brazil🇧🇷', 'Germany🇩🇪', 'Denmark🇩🇰'],
-        datasets: [{
-            label: 'Invoice Total',
-            data: [4667.06, 2689.96, 1722.10, 1677.10, 1392.48, 334.62],
-            backgroundColor: ['red', 'lightgreen', 'lightblue','yellow','lime','pink'],
-        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                position: 'bottom'
+            }
+        }
     }
 });
